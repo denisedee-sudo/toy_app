bf402994102d57ac88e0dd0ecf50ce82efdfa78e
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-09 13:02:27 +0800

    added seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,5 @@
 Rails.application.routes.draw do
-  get 'microposts/index'
-  get 'microposts/show'
-  get 'microposts/new'
-  get 'microposts/create'
-  get 'microposts/edit'
-  get 'microposts/update'
-  get 'microposts/delete'
   resources :microposts
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'user/index'
-  get 'user/show'
-  get 'user/new'
-  get 'user/edit'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
