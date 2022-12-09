2fbe96b75b055a80cc2e74063fddbb67e8e72c7c
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-09 13:21:31 +0800

    added links to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+  end
+  def destroy
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+    end
   end
 end
