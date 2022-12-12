c70aef93502de33244b2cf2526feec84d69c7cc3
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-13 03:15:10 +0800

    fixed create bug

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,14 +1,22 @@
 class MicropostsController < ApplicationController
   def index
+    @microposts = Micropost.all
   end
 
   def show
   end
 
   def new
+    @micropost = Micropost.new
   end
 
   def create
+    @micropost = Micropost.new(microposts_params)
+    if @micropost.save
+      redirect_to @micropost
+    else
+      render "new"
+    end
   end
 
   def edit
@@ -17,6 +25,9 @@ class MicropostsController < ApplicationController
   def update
   end
 
-  def delete
+  def destroy
+  end
+  def microposts_params
+    params.require(:micropost).permit(:content, :user_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,23 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
 
   def edit
   end
-  def destroy
-    @user.destroy
 
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-    end
+  def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
