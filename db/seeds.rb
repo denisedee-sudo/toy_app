c2df9fa9fd66796a7d4aaaae5d8344aec37fdf23
Denise De Jesus <denisedeedejesus@pm.me>
2022-12-19 00:30:04 +0800

    password database encryption

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #   
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-    users = User.create([{ username: "Ise", password: "password", first_name: "Denise" , last_name: "De Jesus", email: "ise@email.com", phone: 9760501920, admin: 1 }, { username: "Derp", password: "dankmemes", first_name: "John" , last_name: "Doe", email: "john@email.com", phone: 9712345678, admin: 0 }])
+    users = User.create([{ username: "Ise", password_digest: "password", first_name: "Denise" , last_name: "De Jesus", email: "ise@email.com", phone: 9760501920, admin: 1 }, { username: "Derp", password_digest: "dankmemes", first_name: "John" , last_name: "Doe", email: "john@email.com", phone: 9712345678, admin: 0 }])
     toys = Toy.create([{ toy_name: 'Lego set', toy_description: 'Modular building blocks' }])
     microposts = Micropost.create([{ content: 'Hello world!', user_id: 1 }])
